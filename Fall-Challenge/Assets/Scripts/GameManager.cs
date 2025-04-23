358e23f2c91e98474be4d4f6989a9ddb21a548d9
VoDanh211200 <duc.macosx.6868@gmail.com>
2025-04-23 21:01:22 +0700

    Game over function and Moving Camera fucntion

--- Fall-Challenge/Assets/Scripts/GameManager.cs
+++ Fall-Challenge/Assets/Scripts/GameManager.cs
@@ -8,9 +8,14 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
     public Text scoreText, livesText;
+    public GameObject ball;
 
     private int score;
-    private int lives =3;
+    private int lives = 3;
+    private Rigidbody2D ballRb;
+    private Vector2 ballStartPos;
+    private float distanceToCamera;
+    private float lastYPos;
 
     void Awake()
     {
@@ -20,12 +25,35 @@ public class GameManager : MonoBehaviour
     void Start()
     {
         UpdateTextElements();
+        ballRb = ball.GetComponent<Rigidbody2D>();
+        ballStartPos = ball.transform.position;
+        distanceToCamera = ballStartPos.y;
+        lastYPos = ballStartPos.y;
+    }
+
+    private void LateUpdate()
+    {
+        if (ball.transform.position.y <= Camera.main.transform.position.y)
+        {
+            Vector3 oldCamPos = Camera.main.transform.position;
+            Vector3 newCamPos = new Vector3(oldCamPos.x, oldCamPos.y - 1f, oldCamPos.z);
+            Camera.main.transform.position = Vector3.Lerp(oldCamPos, newCamPos, 2f * Time.deltaTime);
+        }
     }
 
     public void GameOver()
     {
+        ballStartPos.y = Camera.main.transform.position.y + distanceToCamera;
+        ball.transform.position = ballStartPos;
+        ballRb.velocity = Vector2.zero;
+
         lives--;
         UpdateTextElements();
+
+        if (lives <= 0)
+        {
+            ballRb.isKinematic = true;
+        }
     }
 
     public void AddScore()
