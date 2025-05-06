2b71810b71fbb469dd614c0eba6f2e11e6996c9b
VoDanh211200 <duc.macosx.6868@gmail.com>
2025-05-06 18:15:08 +0700

    Fix bug load level

--- Fall-Challenge/Assets/Scripts/MenuElement.cs
+++ Fall-Challenge/Assets/Scripts/MenuElement.cs
@@ -6,6 +6,23 @@ using UnityEngine.SceneManagement;
 public class MenuElement : MonoBehaviour
 {
     public string sceneToLoad;
+
+    void Start()
+    {
+        string currentSceneName = SceneManager.GetActiveScene().name;
+
+        if (currentSceneName.Contains("Level"))
+        {
+            string levelNumberString = currentSceneName.Substring("Level".Length);
+
+            if (int.TryParse(levelNumberString, out int levelNumber))
+            {
+                int nextLevel = levelNumber + 1;
+                sceneToLoad = "Level" + nextLevel.ToString();
+            }
+        }
+    }
+
     public void RestartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
